228dc9b1cfc2c97cc326e67d2610f0f461da14a7
vitozs <94878000+vitozs@users.noreply.github.com>
2023-02-01 12:28:49 -0300

    Correção na responsividade do menu

--- script/menu.js
+++ script/menu.js
@@ -1,12 +1,23 @@
+let largura = null
+let menu = document.querySelector('.menu-mobile')
+let button = document.getElementById('hamb-menu')
 function menuMobile(){
-    let menu = document.querySelector('.menu-mobile')
-    let button = document.getElementById('hamb-menu')
-    if(window.innerWidth > 800 && menu.classList.contains('open')){
+
+    if(menu.classList.contains('open')){
         menu.classList.remove('open')
         button.className = 'bi bi-list'
     }else{
         menu.classList.add('open')
         button.className = 'bi bi-x-lg'
+
+    }
+}
+
+function getWidth(){
+    largura = window.screen.width
+    if(largura > 800){
+        menu.classList.remove('open')
+        button.className = 'bi bi-list'
 
     }
 }
